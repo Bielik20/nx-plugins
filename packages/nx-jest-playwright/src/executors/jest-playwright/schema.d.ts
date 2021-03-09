646908f0bac99616df7d9546a0951ef0c00bb05e
Damian Bielecki <bielik20@gmail.com>
2021-03-09 16:03:14 +0100

    chore: add jest-playwright executor

--- packages/nx-jest-playwright/src/executors/jest-playwright/schema.d.ts
+++ packages/nx-jest-playwright/src/executors/jest-playwright/schema.d.ts
@@ -1,1 +1,11 @@
-export interface BuildExecutorSchema {} // eslint-disable-line
+import { JestExecutorOptions } from '@nrwl/jest/src/executors/jest/schema';
+
+export interface JestPlaywrightExecutorSchema extends JestExecutorOptions {
+  devServerTarget: string;
+  baseUrl: string;
+  slowMo: number;
+  devtools: boolean;
+  headless: boolean;
+  browsers: ('chromium' | 'firefox' | 'webkit')[];
+  timeout: number;
+}
