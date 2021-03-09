e12cd2bfcde20b40490e2a1aac632060d38c09ce
Damian Bielecki <bielik20@gmail.com>
2021-03-09 12:13:04 +0100

    chore: complete generator

--- packages/nx-jest-playwright/src/generators/project/generator.ts
+++ packages/nx-jest-playwright/src/generators/project/generator.ts
@@ -1,33 +1,61 @@
 import {
-  addProjectConfiguration, formatFiles, generateFiles, names, offsetFromRoot, Tree
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  names,
+  offsetFromRoot,
+  Tree,
 } from '@nrwl/devkit';
+import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import * as path from 'path';
+import { addLinting } from './lib/add-linting';
 import { NormalizedSchema, normalizeOptions } from './lib/normalize-options';
 import { NxJestPlaywrightGeneratorSchema } from './schema';
+
+export default async function (host: Tree, options: NxJestPlaywrightGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(host, options);
+
+  addProjectConfiguration(host, normalizedOptions.projectName, {
+    root: normalizedOptions.projectRoot,
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    projectType: 'application',
+    targets: {
+      e2e: {
+        executor: '@ns3/nx-jest-playwright:build',
+        options: {
+          devServerTarget: `${options.project}:serve`,
+          jestConfig: `${normalizedOptions.projectRoot}/jest.config.js`,
+          passWithNoTests: true,
+        },
+        configurations: {
+          production: {
+            devServerTarget: `${options.project}:serve:production`,
+          },
+        },
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+    implicitDependencies: options.project ? [options.project] : undefined,
+  });
+
+  addFiles(host, normalizedOptions);
+
+  const lintTask = await addLinting(host, normalizedOptions);
+
+  if (!options.skipFormat) {
+    await formatFiles(host);
+  }
+
+  return runTasksInSerial(lintTask);
+}
 
 function addFiles(host: Tree, options: NormalizedSchema) {
   const templateOptions = {
     ...options,
     ...names(options.name),
     offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
+    tmpl: '',
   };
-  generateFiles(host, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
-}
 
-export default async function (host: Tree, options: NxJestPlaywrightGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(host, options);
-  addProjectConfiguration(host, normalizedOptions.projectName, {
-    root: normalizedOptions.projectRoot,
-    projectType: 'library',
-    sourceRoot: `${normalizedOptions.projectRoot}/src`,
-    targets: {
-      build: {
-        executor: '@ns3/nx-jest-playwright:build',
-      },
-    },
-    tags: normalizedOptions.parsedTags,
-  });
-  addFiles(host, normalizedOptions);
-  await formatFiles(host);
+  generateFiles(host, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
 }
