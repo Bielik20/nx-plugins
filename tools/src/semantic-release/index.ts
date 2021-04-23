9011a634e202a4bbe7f4ed073979e0cd319af257
Damian Bielecki <bielik20@gmail.com>
2021-04-23 18:16:04 +0200

    chore: update release workflow

--- tools/src/semantic-release/index.ts
+++ tools/src/semantic-release/index.ts
@@ -1,9 +1,15 @@
-import {
-  runAffectedBuild,
-  runAffectedPublish,
-  runAffectedVersion,
-} from './helpers';
-import { SemanticMethod } from './types';
+import { runAffectedBuild, runAffectedPublish, runAffectedVersion } from './helpers';
+import { SemanticContext, SemanticMethod } from './types';
+import * as core from '@actions/core';
+import * as SemanticReleaseError from '@semantic-release/error';
+
+export const verifyRelease: SemanticMethod = async (config, context) => {
+  if (context.options.failure === 'true' || context.options.failure === true) {
+    throw new SemanticReleaseError('Release failed outside semantic release process!');
+  }
+
+  setGitHubActionsOutputs(context);
+};
 
 export const prepare: SemanticMethod = async (config, context) => {
   await runAffectedVersion(context);
@@ -13,3 +19,8 @@ export const prepare: SemanticMethod = async (config, context) => {
 export const publish: SemanticMethod = async (config, context) => {
   await runAffectedPublish(context);
 };
+
+export function setGitHubActionsOutputs(context: SemanticContext): void {
+  core.setOutput('next-version', context.nextRelease.version);
+  core.setOutput('last-head', context.lastRelease.gitHead);
+}
