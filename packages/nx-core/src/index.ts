6cde975fac48d25e83033d35abdace3c31890398
Damian Bielecki <bielik20@gmail.com>
2021-04-24 09:28:21 +0200

    fix: remove @nrwl/nx-plugin as a dependency

--- packages/nx-core/src/index.ts
+++ packages/nx-core/src/index.ts
@@ -1,4 +1,3 @@
 export * from './utils/stringify-args';
 export * from './utils/exec-process';
 export * from './utils/get-project-configuration';
-export * from './testing-utils/run-nx-new-command';
