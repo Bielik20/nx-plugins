c88bd852e4530565b06bab7534bbf0a2c5037762
Damian Bielecki <bielik20@gmail.com>
2021-06-25 10:48:56 +0200

    chore: run migrations

--- packages/nx-jest-playwright/jest.config.js
+++ packages/nx-jest-playwright/jest.config.js
@@ -2,13 +2,12 @@ module.exports = {
   displayName: 'nx-jest-playwright',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    },
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/nx-jest-playwright',
+  testEnvironment: 'node',
 };
