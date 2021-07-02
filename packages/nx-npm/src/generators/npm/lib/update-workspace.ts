fd87700784c6f5ac99712f6134fd030b39bdb47d
Damian Bielecki <bielik20@gmail.com>
2021-07-02 18:09:49 +0200

    chore(npm): remove version executor from generator
    You should use publish generator for that

--- packages/nx-npm/src/generators/npm/lib/update-workspace.ts
+++ packages/nx-npm/src/generators/npm/lib/update-workspace.ts
@@ -4,9 +4,6 @@ import { NxNpmGeneratorSchema } from '../schema';
 export function updateWorkspace(tree: Tree, options: NxNpmGeneratorSchema) {
   const projectConfig = readProjectConfiguration(tree, options.project);
 
-  projectConfig.targets.version = {
-    executor: '@ns3/nx-npm:version',
-  };
   projectConfig.targets.publish = {
     executor: '@ns3/nx-npm:publish',
   };
