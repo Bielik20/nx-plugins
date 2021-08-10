8e9f4ad9e72e979c709979a8f56c259c70c9b15e
Damian Bielecki <bielik20@gmail.com>
2021-08-10 19:08:39 +0200

    chore: run nx migrations

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-  projects: [
-    '<rootDir>/packages/nx-core',
-    '<rootDir>/e2e/nx-core-e2e',
-    '<rootDir>/packages/nx-serverless',
-    '<rootDir>/e2e/nx-serverless-e2e',
-    '<rootDir>/packages/nx-jest-playwright',
-    '<rootDir>/e2e/nx-jest-playwright-e2e',
-    '<rootDir>/packages/nx-npm',
-    '<rootDir>/e2e/nx-npm-e2e',
-  ],
+  projects: [...getJestProjects()],
 };
