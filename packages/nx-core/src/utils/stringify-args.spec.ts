10efcee7c7c41e3c89d0ec57b3813102522d8d48
Dave Lowther <dave@davelowther.net>
2021-10-23 14:32:31 -0400

    test(nx-core): add test cases for stringified flags

---
+++ packages/nx-core/src/utils/stringify-args.spec.ts
@@ -0,0 +1,38 @@
+import { stringifyArgs } from './stringify-args';
+
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+type TestOpt = Record<string, any>;
+test('it should add flag arguments when passed a boolean true', () => {
+  const options: TestOpt = {
+    verbose: true,
+  };
+
+  const expected = '--verbose';
+  const result = stringifyArgs(options);
+
+  expect(result).toBe(expected);
+});
+
+test('it should add flag arguments when in mixed grouping', () => {
+  const options: TestOpt = {
+    stage: 'dev',
+    verbose: true,
+  };
+
+  const expected = '--stage=dev --verbose';
+  const result = stringifyArgs(options);
+
+  expect(result).toBe(expected);
+});
+
+test('it should handle a string true as a normal argument', () => {
+  const options: TestOpt = {
+    stage: 'dev',
+    someOption: 'true',
+  };
+
+  const expected = '--stage=dev --someOption=true';
+  const result = stringifyArgs(options);
+
+  expect(result).toBe(expected);
+});
