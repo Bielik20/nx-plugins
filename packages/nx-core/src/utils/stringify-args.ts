0ad1e2a07a7fe9343cc947829313a7197a88c259
Dave Lowther <dave@davelowther.net>
2021-10-23 14:35:31 -0400

    feat(nx-core): update to handle cli flags without values

--- packages/nx-core/src/utils/stringify-args.ts
+++ packages/nx-core/src/utils/stringify-args.ts
@@ -1,6 +1,7 @@
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function stringifyArgs(options: Record<string, any>): string {
   return Object.keys(options)
     .filter((key) => options[key] !== undefined)
-    .map((key) => `--${key}=${options[key]}`)
+    .map((key) => (options[key] === true ? `--${key}` : `--${key}=${options[key]}`))
     .join(' ');
 }
