263411a9e79a3d45311cce95a10a05037bc19221
Dave Lowther <dave@davelowther.net>
2021-10-23 14:38:29 -0400

    test(nx-core): add test case for normal key/val args

--- packages/nx-core/src/utils/stringify-args.spec.ts
+++ packages/nx-core/src/utils/stringify-args.spec.ts
@@ -2,6 +2,18 @@ import { stringifyArgs } from './stringify-args';
 
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TestOpt = Record<string, any>;
+
+test('it should add key/value arguments correctly', () => {
+  const options: TestOpt = {
+    stage: 'dev',
+  };
+
+  const expected = '--stage=dev';
+  const result = stringifyArgs(options);
+
+  expect(result).toBe(expected);
+});
+
 test('it should add flag arguments when passed a boolean true', () => {
   const options: TestOpt = {
     verbose: true,
