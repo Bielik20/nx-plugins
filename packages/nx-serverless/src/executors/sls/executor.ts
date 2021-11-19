69959870a9468013e981b8974ffd6fc615547928
Damian Bielecki <bielik20@gmail.com>
2021-11-19 19:42:41 +0100

    feat(serverless): add universal sls executor

---
+++ packages/nx-serverless/src/executors/sls/executor.ts
@@ -0,0 +1,29 @@
+import { ExecutorContext } from '@nrwl/devkit';
+import { getProjectConfiguration, stringifyArgs } from '@ns3/nx-core';
+import { execSync } from 'child_process';
+import { printCommand } from '../../utils/print-command';
+import { SlsExecutorSchema } from './schema';
+
+export default async function runExecutor(options: SlsExecutorSchema, context: ExecutorContext) {
+  const { showHelp, command, env = process.env, ...rest } = options;
+  const projectRoot = getProjectConfiguration(context).root;
+  const stringifiedArgs = stringifyArgs({
+    ...rest,
+    ...(showHelp ? { help: true } : {}),
+  });
+  const fullCommand = `sls ${command} ${stringifiedArgs}`.trim();
+
+  printCommand(fullCommand);
+  execSync(fullCommand, {
+    cwd: projectRoot,
+    stdio: 'inherit',
+    env: {
+      ...env,
+      nxContext: JSON.stringify(context),
+    },
+  });
+
+  return {
+    success: true,
+  };
+}
