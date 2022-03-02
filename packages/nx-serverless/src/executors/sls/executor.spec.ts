bd673aa63d67c3ee68153ca657082c87e52bc6c2
Damian Bielecki <bielik20@gmail.com>
2022-03-02 17:40:33 +0100

    feat(serverless): console log entire output in CI
    Nx Cloud doesn't pick up log when run with inherit.
    stdio inherit will be used in local run where user can inspect output.
    console.log will print results in CI thus appear in NxCloud output.

--- packages/nx-serverless/src/executors/sls/executor.spec.ts
+++ packages/nx-serverless/src/executors/sls/executor.spec.ts
@@ -1,4 +1,4 @@
-import * as childProcess from 'child_process';
+import * as execa from 'execa';
 import { NX_CONTEXT_KEY } from '../../../plugin/nrwl/nx-constants';
 import { testContext } from '../../utils/test-context';
 import executor from './executor';
@@ -6,74 +6,101 @@ import executor from './executor';
 const runCommandsReturn = { success: true };
 
 describe('Sls Executor', () => {
-  const execSyncMock = jest.spyOn(childProcess, 'execSync').mockReturnValue(null);
+  let execSyncMock: jest.SpyInstance;
 
   beforeEach(() => {
     jest.resetAllMocks();
+    execSyncMock = jest.spyOn(execa, 'command').mockResolvedValue({ all: 'noop' } as any);
   });
 
-  it('can run', async () => {
-    const output = await executor({ command: 'package' }, testContext);
+  describe('local run', () => {
+    beforeEach(() => {
+      process.env.CI = 'false';
+    });
 
-    expect(output).toEqual(runCommandsReturn);
-    expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
-      cwd: 'apps/serverless839554',
-      stdio: 'inherit',
-      env: expect.anything(),
+    it('can run', async () => {
+      const output = await executor({ command: 'package' }, testContext);
+
+      expect(output).toEqual(runCommandsReturn);
+      expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
+        all: false,
+        cwd: 'apps/serverless839554',
+        stdio: 'inherit',
+        env: expect.anything(),
+      });
+    });
+
+    it('should pass inline arguments', async () => {
+      const output = await executor(
+        { foo: 'foo-value', bar: 'bar-value', command: 'deploy' },
+        testContext,
+      );
+
+      expect(output).toEqual(runCommandsReturn);
+      expect(execSyncMock).toHaveBeenCalledWith('npx sls deploy --foo=foo-value --bar=bar-value', {
+        all: false,
+        cwd: 'apps/serverless839554',
+        stdio: 'inherit',
+        env: expect.anything(),
+      });
+    });
+
+    it('should include nx context in env', async () => {
+      await executor({ command: 'package' }, testContext);
+      const { env } = execSyncMock.mock.calls[0][1];
+
+      expect(env[NX_CONTEXT_KEY]).toBe(JSON.stringify(testContext));
+    });
+
+    it('should overwrite env', async () => {
+      const fakeEnv = { foo: 'bar' };
+      const output = await executor({ command: 'package', env: fakeEnv }, testContext);
+      const fakeEnvWithNxContext = {
+        ...fakeEnv,
+        FORCE_COLOR: 'true',
+        [NX_CONTEXT_KEY]: JSON.stringify(testContext),
+        NODE_OPTIONS: '--enable-source-maps',
+      };
+
+      expect(output).toEqual(runCommandsReturn);
+      expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
+        all: false,
+        cwd: 'apps/serverless839554',
+        stdio: 'inherit',
+        env: fakeEnvWithNxContext,
+      });
+    });
+
+    it('env should overwrite NODE_OPTIONS', async () => {
+      const fakeEnv = { foo: 'bar', NODE_OPTIONS: undefined };
+      const output = await executor({ command: 'package', env: fakeEnv }, testContext);
+      const fakeEnvWithNxContext = {
+        ...fakeEnv,
+        FORCE_COLOR: 'true',
+        [NX_CONTEXT_KEY]: JSON.stringify(testContext),
+      };
+
+      expect(output).toEqual(runCommandsReturn);
+      expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
+        all: false,
+        cwd: 'apps/serverless839554',
+        stdio: 'inherit',
+        env: fakeEnvWithNxContext,
+      });
     });
   });
 
-  it('should pass inline arguments', async () => {
-    const output = await executor(
-      { foo: 'foo-value', bar: 'bar-value', command: 'deploy' },
-      testContext,
-    );
-
-    expect(output).toEqual(runCommandsReturn);
-    expect(execSyncMock).toHaveBeenCalledWith('npx sls deploy --foo=foo-value --bar=bar-value', {
-      cwd: 'apps/serverless839554',
-      stdio: 'inherit',
-      env: expect.anything(),
+  describe('CI run', () => {
+    beforeEach(() => {
+      process.env.CI = 'true';
     });
-  });
 
-  it('should include nx context in env', async () => {
-    await executor({ command: 'package' }, testContext);
-    const { env } = execSyncMock.mock.calls[0][1];
+    it('should console.log all output', async () => {
+      const consoleLogMock = jest.spyOn(console, 'log');
 
-    expect(env[NX_CONTEXT_KEY]).toBe(JSON.stringify(testContext));
-  });
+      await executor({ command: 'package' }, testContext);
 
-  it('should overwrite env', async () => {
-    const fakeEnv = { foo: 'bar' };
-    const output = await executor({ command: 'package', env: fakeEnv }, testContext);
-    const fakeEnvWithNxContext = {
-      ...fakeEnv,
-      [NX_CONTEXT_KEY]: JSON.stringify(testContext),
-      NODE_OPTIONS: '--enable-source-maps',
-    };
-
-    expect(output).toEqual(runCommandsReturn);
-    expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
-      cwd: 'apps/serverless839554',
-      stdio: 'inherit',
-      env: fakeEnvWithNxContext,
-    });
-  });
-
-  it('env should overwrite NODE_OPTIONS', async () => {
-    const fakeEnv = { foo: 'bar', NODE_OPTIONS: undefined };
-    const output = await executor({ command: 'package', env: fakeEnv }, testContext);
-    const fakeEnvWithNxContext = {
-      ...fakeEnv,
-      [NX_CONTEXT_KEY]: JSON.stringify(testContext),
-    };
-
-    expect(output).toEqual(runCommandsReturn);
-    expect(execSyncMock).toHaveBeenCalledWith('npx sls package', {
-      cwd: 'apps/serverless839554',
-      stdio: 'inherit',
-      env: fakeEnvWithNxContext,
+      expect(consoleLogMock).toHaveBeenCalledWith('noop');
     });
   });
 });
--- packages/nx-serverless/src/executors/sls/executor.ts
+++ packages/nx-serverless/src/executors/sls/executor.ts
@@ -1,12 +1,14 @@
+import 'dotenv/config';
 import { ExecutorContext } from '@nrwl/devkit';
 import { getProjectConfiguration, stringifyArgs } from '@ns3/nx-core';
-import { execSync } from 'child_process';
+import * as execa from 'execa';
 import { NX_BUILD_TARGET_KEY, NX_CONTEXT_KEY } from '../../../plugin/nrwl/nx-constants';
 import { printCommand } from '../../utils/print-command';
 import { SlsExecutorSchema } from './schema';
 
 export default async function runExecutor(options: SlsExecutorSchema, context: ExecutorContext) {
   const { showHelp, buildTarget, command, env = process.env, ...rest } = options;
+  const IS_CI_RUN = process.env.CI === 'true';
   const projectRoot = getProjectConfiguration(context).root;
   const stringifiedArgs = stringifyArgs({
     ...rest,
@@ -15,16 +17,22 @@ export default async function runExecutor(options: SlsExecutorSchema, context: E
   const fullCommand = `npx sls ${command} ${stringifiedArgs}`.trim();
 
   printCommand(fullCommand);
-  execSync(fullCommand, {
+  const result = await execa.command(fullCommand, {
     cwd: projectRoot,
-    stdio: 'inherit',
+    stdio: IS_CI_RUN ? undefined : 'inherit',
+    all: IS_CI_RUN,
     env: {
+      FORCE_COLOR: 'true',
       NODE_OPTIONS: '--enable-source-maps',
       ...env,
       [NX_CONTEXT_KEY]: JSON.stringify(context),
       [NX_BUILD_TARGET_KEY]: buildTarget,
     },
   });
+
+  if (IS_CI_RUN) {
+    console.log(result.all);
+  }
 
   return {
     success: true,
