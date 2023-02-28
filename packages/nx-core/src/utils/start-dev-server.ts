c1cb5a090216cb10d4da1f4fbf71791610395aef
Damian Bielecki <bielik20@gmail.com>
2023-02-28 20:04:30 +0100

    feat(core): extract startDevServer to @ns3/nx-core

---
+++ packages/nx-core/src/utils/start-dev-server.ts
@@ -0,0 +1,47 @@
+import { ExecutorContext, parseTargetString, readTargetOptions, runExecutor } from '@nrwl/devkit';
+
+export interface StartDevServerOptions {
+  devServerTarget?: string;
+  skipServe?: boolean;
+  baseUrl?: string;
+  watch?: boolean;
+  watchAll?: boolean;
+}
+
+/**
+ * @link https://github.com/nrwl/nx/blob/94dffd1e922eaf0b3e15c3acb4ccab90ce443643/packages/cypress/src/executors/cypress/cypress.impl.ts#L153-L197
+ */
+export async function* startDevServer(opts: StartDevServerOptions, context: ExecutorContext) {
+  const watch = opts.watch || opts.watchAll;
+  // no dev server, return the provisioned base url
+  if (!opts.devServerTarget || opts.skipServe) {
+    yield opts.baseUrl;
+    return;
+  }
+
+  const { project, target, configuration } = parseTargetString(
+    opts.devServerTarget,
+    context.projectGraph,
+  );
+  const devServerTargetOpts = readTargetOptions({ project, target, configuration }, context);
+  const targetSupportsWatchOpt = Object.keys(devServerTargetOpts).includes('watch');
+
+  for await (const output of await runExecutor<{
+    success: boolean;
+    baseUrl?: string;
+    info?: { port: number; baseUrl?: string };
+  }>(
+    { project, target, configuration },
+    // @NOTE: Do not forward watch option if not supported by the target dev server,
+    // this is relevant for running Cypress against dev server target that does not support this option,
+    // for instance @nguniversal/builders:ssr-dev-server.
+    targetSupportsWatchOpt ? { watch } : {},
+    context,
+  )) {
+    if (!output.success && !watch) throw new Error('Could not compile application files');
+    if (!opts.baseUrl && !output.baseUrl && !output.info?.baseUrl && output.info?.port) {
+      output.baseUrl = `http://localhost:${output.info.port}`;
+    }
+    yield opts.baseUrl || output.baseUrl || output.info?.baseUrl;
+  }
+}
