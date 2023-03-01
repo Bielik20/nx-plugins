063119e601d8568bdd24cfc5590c6e37a25d1fd3
Damian Bielecki <bielik20@gmail.com>
2023-03-01 22:59:56 +0100

    test(playwright): adjust e2e

--- e2e/nx-playwright-e2e/tests/nx-playwright.spec.ts
+++ e2e/nx-playwright-e2e/tests/nx-playwright.spec.ts
@@ -1,23 +1,24 @@
 import {
   checkFilesExist,
-  ensureNxProject,
   readJson,
+  runCommandAsync,
   runNxCommandAsync,
   uniq,
 } from '@nrwl/nx-plugin/testing';
+import { ensureComplexNxProject } from '@ns3/nx-core/src/testing-utils/ensure-complex-nx-project';
+import * as assert from 'node:assert/strict';
+import { after, before, describe, it } from 'node:test';
 
 describe('nx-playwright e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
-  beforeAll(() => {
-    ensureNxProject('@ns3/nx-playwright', 'dist/packages/nx-playwright');
+  before(async () => {
+    ensureComplexNxProject(
+      ['@ns3/nx-playwright', 'dist/packages/nx-playwright'],
+      ['@ns3/nx-core', 'dist/packages/nx-core'],
+    );
+    await runCommandAsync('npx playwright install chromium');
   });
 
-  afterAll(() => {
+  after(() => {
     // `nx reset` kills the daemon, and performs
     // some work which can help clean up e2e leftovers
     runNxCommandAsync('reset');
@@ -25,30 +26,34 @@ describe('nx-playwright e2e', () => {
 
   it('should create nx-playwright', async () => {
     const project = uniq('nx-playwright');
-    await runNxCommandAsync(`generate @ns3/nx-playwright:nx-playwright ${project}`);
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
-  }, 120000);
+    await runNxCommandAsync(
+      `generate @ns3/nx-playwright:project ${project} --baseUrl https://playwright.dev/`,
+    );
+    const result = await runNxCommandAsync(`e2e ${project}`, { silenceError: true });
+    const sanitised = (result.stdout + result.stderr).replace(/[^\w\n]/g, ' ');
+
+    assert.match(sanitised, /1 passed/);
+    assert.match(sanitised, /Successfully ran target e2e for project/);
+  });
 
   describe('--directory', () => {
     it('should create src in the specified directory', async () => {
       const project = uniq('nx-playwright');
-      await runNxCommandAsync(
-        `generate @ns3/nx-playwright:nx-playwright ${project} --directory subdir`,
-      );
-      expect(() => checkFilesExist(`libs/subdir/${project}/src/index.ts`)).not.toThrow();
-    }, 120000);
+      await runNxCommandAsync(`generate @ns3/nx-playwright:project ${project} --directory subdir`);
+
+      assert.doesNotThrow(() => checkFilesExist(`apps/subdir/${project}/tests/app.spec.ts`));
+    });
   });
 
   describe('--tags', () => {
     it('should add tags to the project', async () => {
       const projectName = uniq('nx-playwright');
-      ensureNxProject('@ns3/nx-playwright', 'dist/packages/nx-playwright');
       await runNxCommandAsync(
-        `generate @ns3/nx-playwright:nx-playwright ${projectName} --tags e2etag,e2ePackage`,
+        `generate @ns3/nx-playwright:project ${projectName} --tags e2etag,e2ePackage`,
       );
-      const project = readJson(`libs/${projectName}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
-    }, 120000);
+      const project = readJson(`apps/${projectName}/project.json`);
+
+      assert.deepEqual(project.tags, ['e2etag', 'e2ePackage']);
+    });
   });
 });
