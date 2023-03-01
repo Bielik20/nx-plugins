b5365fa0618b21e8a24a8c75e81bb503e9ab43d8
Damian Bielecki <bielik20@gmail.com>
2023-03-01 18:32:31 +0100

    chore: generate nx-playwright

---
+++ packages/nx-playwright/src/generators/nx-playwright/generator.spec.ts
@@ -0,0 +1,19 @@
+import { readProjectConfiguration, Tree } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import generator from './generator';
+import { NxPlaywrightGeneratorSchema } from './schema';
+
+describe('nx-playwright generator', () => {
+  let appTree: Tree;
+  const options: NxPlaywrightGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
