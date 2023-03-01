cd7e30ce1c825acf6ad78345fafdd69b7fa5c00c
Damian Bielecki <bielik20@gmail.com>
2023-03-01 19:15:13 +0100

    feat(playwright): add generators

--- packages/nx-playwright/src/generators/project/schema.d.ts
+++ packages/nx-playwright/src/generators/project/schema.d.ts
@@ -1,9 +1,14 @@
+import { Linter } from '@nrwl/linter';
 import { NormalizedOptions } from '@ns3/nx-core';
 
 export interface PlaywrightGeneratorSchema {
   name: string;
-  tags?: string;
   directory?: string;
+  linter?: Linter;
+  project?: string;
+  baseUrl?: string;
+  skipPackageJson?: boolean;
+  skipFormat?: boolean;
 }
 
 export interface PlaywrightGeneratorNormalizedSchema
