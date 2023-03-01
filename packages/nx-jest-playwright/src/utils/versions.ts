76c553444742205b8a1966d9ed44563a93d2f511
Damian Bielecki <bielik20@gmail.com>
2023-03-01 11:19:04 +0100

    refactor(core): cleanup unused stuff from @ns3/nx-core
    BREAKING CHANGE: remove
    - execProcess
    - getPackageManagerInstall
    - getPackageManagerLockFile
    - ProcessOutput, log, data
    - spawnProcess
    - dependencies, devDependencies

---
+++ packages/nx-jest-playwright/src/utils/versions.ts
@@ -0,0 +1,2 @@
+export const jestPlaywrightPresetVersion = '^2.0.0';
+export const playwrightVersion = '^1.30.0';
---
+++ packages/nx-serverless/src/utils/versions.ts
@@ -0,0 +1,3 @@
+export const serverlessVersion = '^3.27.0';
+export const serverlessBundleVersion = '^6.0.0';
+export const serverlessOfflineVersion = '^12.0.4';
