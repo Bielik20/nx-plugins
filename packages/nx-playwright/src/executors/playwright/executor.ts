6213f212db7f57e0945ec2d524e979e1ae5ec209
Damian Bielecki <bielik20@gmail.com>
2023-03-02 16:34:05 +0100

    feat(playwright): add executor

--- packages/nx-playwright/src/executors/playwright/executor.ts
+++ packages/nx-playwright/src/executors/playwright/executor.ts
@@ -1,8 +1,46 @@
+import { ExecutorContext, getPackageManagerCommand, logger } from '@nrwl/devkit';
+import { printCommand, startDevServer, stringifyArgs } from '@ns3/nx-core';
+import * as execa from 'execa';
 import { PlaywrightExecutorSchema } from './schema';
 
-export default async function runExecutor(options: PlaywrightExecutorSchema) {
-  console.log('Executor ran for Build', options);
-  return {
-    success: true,
-  };
+export default async function runExecutor(
+  options: PlaywrightExecutorSchema,
+  context: ExecutorContext,
+) {
+  const { devServerTarget, command, skipServe, baseUrl, ...rest } = options;
+  const watch = false; // not supported;
+  const devServerOptions = { devServerTarget, skipServe, baseUrl, watch } as const;
+  const pmc = getPackageManagerCommand();
+  const args = stringifyArgs(rest, { shorthand: true });
+  const fullCommand = `${pmc.exec} ${command} ${args}`.trim();
+
+  let success: boolean;
+  for await (const baseUrl of startDevServer(devServerOptions, context)) {
+    try {
+      success = await runPlaywright(fullCommand, baseUrl, context);
+      if (!watch) break;
+    } catch (e) {
+      logger.error(e.message);
+      success = false;
+      if (!watch) break;
+    }
+  }
+
+  return { success };
+}
+
+function runPlaywright(fullCommand: string, baseUrl: string, context: ExecutorContext) {
+  printCommand(fullCommand);
+  return execa
+    .command(fullCommand, {
+      cwd: context.root,
+      stdio: 'inherit',
+      env: {
+        FORCE_COLOR: 'true',
+        BASE_URL: baseUrl,
+        ...process.env,
+      },
+    })
+    .then(() => true)
+    .catch(() => false);
 }
