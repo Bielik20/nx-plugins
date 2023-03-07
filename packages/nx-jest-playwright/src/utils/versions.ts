6369a348b6ce2873d84a9ed75f4a6e2ee49307eb
Damian Bielecki <bielik20@gmail.com>
2023-03-07 16:40:06 +0100

    feat(jest-playwright): bump jestPlaywrightPresetVersion

--- packages/nx-jest-playwright/src/utils/versions.ts
+++ packages/nx-jest-playwright/src/utils/versions.ts
@@ -1,2 +1,2 @@
-export const jestPlaywrightPresetVersion = '^2.0.0';
-export const playwrightVersion = '^1.30.0';
+export const jestPlaywrightPresetVersion = '^3.0.0';
+export const playwrightVersion = '^1.31.2';
