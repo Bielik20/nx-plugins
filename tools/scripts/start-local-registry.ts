4fd623c5cce88ecd9869c1faac7c74ab050672c7
Damian Bielecki <bielik20@gmail.com>
2023-09-11 14:05:27 +0200

    test: adjust start-local-registry script

--- tools/scripts/start-local-registry.ts
+++ tools/scripts/start-local-registry.ts
@@ -2,6 +2,7 @@
  * This script starts a local registry for e2e testing purposes.
  * It is meant to be called in jest's globalSetup.
  */
+import { readCachedProjectGraph } from '@nx/devkit';
 import { startLocalRegistry } from '@nx/js/plugins/jest/local-registry';
 import { execFileSync } from 'child_process';
 
@@ -10,15 +11,34 @@ export default async () => {
   const localRegistryTarget = '@ns3/nx-plugins:local-registry';
   // storage folder for the local registry
   const storage = './tmp/local-registry/storage';
+  const graph = readCachedProjectGraph();
+  const targetE2eProjectName = process.env['NX_TASK_TARGET_PROJECT'];
+  const targetE2eProjectDeps = graph.nodes[targetE2eProjectName].data.implicitDependencies || [];
 
+  // sets process.env.npm_config_registry which is used by publish target
   global.stopLocalRegistry = await startLocalRegistry({
     localRegistryTarget,
     storage,
     verbose: false,
   });
   const nx = require.resolve('nx');
-  execFileSync(nx, ['run-many', '--targets', 'publish', '--ver', '1.0.0', '--tag', 'e2e'], {
-    env: process.env,
-    stdio: 'inherit',
-  });
+  execFileSync(
+    nx,
+    [
+      'run-many',
+      '--targets',
+      'publish',
+      '--projects',
+      targetE2eProjectDeps.join(','),
+    ],
+    {
+      env: {
+        ...process.env,
+        NPM_TOKEN: 'fake-token',
+        NPM_PACKAGE_VERSION: '0.0.0-e2e',
+        NPM_PACKAGE_TAG: 'e2e',
+      },
+      stdio: 'inherit',
+    },
+  );
 };
