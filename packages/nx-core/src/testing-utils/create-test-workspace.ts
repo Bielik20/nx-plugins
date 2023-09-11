d734014eb77ababab5eb89d5e8cd55a78253bca9
Damian Bielecki <bielik20@gmail.com>
2023-09-11 14:05:27 +0200

    test: adjust start-local-registry script

---
+++ packages/nx-core/src/testing-utils/create-test-workspace.ts
@@ -0,0 +1,56 @@
+import { workspaceRoot } from '@nx/devkit';
+import { execSync } from 'child_process';
+import { mkdirSync, rmSync } from 'fs';
+import { dirname, join } from 'path';
+
+export function createTestWorkspace(packageName: string) {
+  const projectDirectory = createTestProject();
+
+  // The plugin has been built and published to a local registry in the jest globalSetup
+  // Install the plugin built with the latest source code into the test repo
+  execSync(`npm install ${packageName}@e2e`, {
+    cwd: projectDirectory,
+    stdio: 'inherit',
+    env: process.env,
+  });
+
+  return projectDirectory;
+}
+
+/**
+ * Creates a test project with create-nx-workspace and installs the plugin
+ * @returns The directory where the test project was created
+ */
+function createTestProject() {
+  // backwards compatibility with runNxCommandAsync etc.
+  const projectName = 'proj';
+  const projectDirectory = join(workspaceRoot, 'tmp', 'nx-e2e', projectName);
+
+  // Ensure projectDirectory is empty
+  rmSync(projectDirectory, {
+    recursive: true,
+    force: true,
+  });
+  mkdirSync(dirname(projectDirectory), {
+    recursive: true,
+  });
+
+  execSync(
+    `npx --yes create-nx-workspace@latest ${projectName} --preset apps --no-nxCloud --no-interactive`,
+    {
+      cwd: dirname(projectDirectory),
+      stdio: 'inherit',
+      env: process.env,
+    },
+  );
+  console.log(`Created test project in "${projectDirectory}"`);
+
+  return projectDirectory;
+}
+
+export function cleanupTestWorkspace(projectDirectory: string) {
+  rmSync(projectDirectory, {
+    recursive: true,
+    force: true,
+  });
+}
