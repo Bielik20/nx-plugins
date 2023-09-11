f76b543d56252787e3a11a8d4a62a81686ff20ea
Damian Bielecki <bielik20@gmail.com>
2023-09-11 13:44:50 +0200

    fix(nx-serverless): use correct output paths

--- packages/nx-serverless/src/generators/application/lib/get-output-path.ts
+++
@@ -1,6 +0,0 @@
-import { NormalizedOptions } from '@ns3/nx-core';
-import { join } from 'path';
-
-export function getOutputPath(options: NormalizedOptions) {
-  return join(options.projectRoot, '.serverless');
-}
