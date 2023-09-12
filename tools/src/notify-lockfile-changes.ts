5f5bd9823a4bce75224624e7a92d5cb9be637942
Damian Bielecki <bielik20@gmail.com>
2023-09-12 15:58:16 +0200

    chore: replace yarn with npm

--- tools/src/notify-lockfile-changes.ts
+++ tools/src/notify-lockfile-changes.ts
@@ -1,11 +1,11 @@
 import { yellow } from 'colorette';
 
-if (process.argv.slice(2).some((arg) => arg.includes('yarn.lock'))) {
+if (process.argv.slice(2).some((arg) => arg.includes('package-lock.json'))) {
   console.warn(
     yellow([
-      '⚠️  ---------------------------------------------------------------------------- ⚠️',
-      '⚠️  yarn.lock changed, please run `yarn` to ensure your packages are up to date. ⚠️',
-      '⚠️  ---------------------------------------------------------------------------- ⚠️',
+      '⚠️  ------------------------------------------------------------------------------------- ⚠️',
+      '⚠️  package-lock.json changed, please run `npm i` to ensure your packages are up to date. ⚠️',
+      '⚠️  ------------------------------------------------------------------------------------- ⚠️',
     ].join('\n'))
   );
 }
