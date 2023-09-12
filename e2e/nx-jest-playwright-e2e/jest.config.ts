e18e4769bcb9a0640be3ca6824b2e6a3fcfbbdd2
Damian Bielecki <bielik20@gmail.com>
2023-09-12 10:28:55 +0200

    test: adjust test to use local registry

--- e2e/nx-jest-playwright-e2e/jest.config.ts
+++ e2e/nx-jest-playwright-e2e/jest.config.ts
@@ -1,11 +1,12 @@
-module.exports = {
+export default {
   displayName: 'nx-jest-playwright-e2e',
   preset: '../../jest.preset.js',
   testTimeout: 120000,
-  globals: {},
   transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/e2e/nx-jest-playwright-e2e',
+  globalSetup: '../../tools/scripts/start-local-registry.ts',
+  globalTeardown: '../../tools/scripts/stop-local-registry.ts',
 };
--- e2e/nx-npm-e2e/jest.config.ts
+++ e2e/nx-npm-e2e/jest.config.ts
@@ -1,11 +1,12 @@
-module.exports = {
+export default {
   displayName: 'nx-npm-e2e',
   preset: '../../jest.preset.js',
   testTimeout: 120000,
-  globals: {},
   transform: {
     '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/e2e/nx-npm-e2e',
+  globalSetup: '../../tools/scripts/start-local-registry.ts',
+  globalTeardown: '../../tools/scripts/stop-local-registry.ts',
 };
