b0511c2431dc0c64c39b53005b8d8e9120108b63
Damian Bielecki <bielik20@gmail.com>
2023-09-18 10:39:10 +0200

    feat(serverless): improve serve

--- packages/nx-serverless/plugin/nrwl/nx-facade.ts
+++ packages/nx-serverless/plugin/nrwl/nx-facade.ts
@@ -1,54 +1,35 @@
-import { ProjectGraph, readCachedProjectGraph, workspaceRoot } from '@nx/devkit';
+import {
+  ExecutorContext,
+  parseTargetString,
+  ProjectGraph,
+  readCachedProjectGraph,
+  readNxJson,
+  readProjectsConfigurationFromProjectGraph,
+  runExecutor,
+  Target,
+  workspaceRoot,
+} from '@nx/devkit';
 import * as execa from 'execa';
 import { join } from 'path';
 
 export const NX_SERVERLESS_BUILD_TARGET_KEY = 'NS3_NX_SERVERLESS_BUILD_TARGET';
 
 export class NxFacade {
+  private readonly graph: ProjectGraph;
   private readonly buildTarget: string;
   readonly outputAbsolutePath: string;
 
   constructor(private logging: Serverless.Logging) {
     try {
-      this.buildTarget = this.getBuildTarget();
-      this.outputAbsolutePath = this.getAbsoluteOutputPath();
+      this.graph = readCachedProjectGraph();
+      this.buildTarget = process.env[NX_SERVERLESS_BUILD_TARGET_KEY] as string;
+      this.outputAbsolutePath = getAbsoluteOutputPath(this.graph, this.buildTarget);
     } catch (e) {
+      console.log(e);
       throw new Error(
         '@nrwl/nx context not found. This is probably because you are running serverless outside nx command.',
       );
     }
-  }
-
-  private getBuildTarget() {
-    return process.env[NX_SERVERLESS_BUILD_TARGET_KEY] as string;
-  }
-
-  private getAbsoluteOutputPath() {
-    const graph = readCachedProjectGraph();
-    const [project, target, configuration] = this.buildTarget.split(':');
-    const outputPath = this.getOption<string>(
-      graph,
-      { project, target, configuration },
-      'outputPath',
-    );
-
-    return join(workspaceRoot, outputPath);
-  }
-
-  private getOption<T>(
-    graph: ProjectGraph,
-    descriptor: { project: string; target: string; configuration: string },
-    option: string,
-  ): T {
-    const { project, target, configuration } = descriptor;
-    const targetConfiguration = graph.nodes[project].data.targets[target];
-    const defaultConfiguration = targetConfiguration.defaultConfiguration;
-    const normalizedConfiguration = configuration ?? defaultConfiguration;
-
-    return (
-      targetConfiguration.options[option] ??
-      targetConfiguration.configurations[normalizedConfiguration]?.[option]
-    );
   }
 
   async build(): Promise<void> {
@@ -75,10 +56,56 @@ export class NxFacade {
 
   async watch(): Promise<void> {
     this.logging.log.info(`Watching with nx buildTarget: "${this.buildTarget}"`);
-    await execa.command(`npx nx run ${this.buildTarget}`, { cwd: workspaceRoot });
-    execa.command(`npx nx run ${this.buildTarget} --watch --skip-nx-cache`, {
-      stdio: 'inherit',
-      cwd: workspaceRoot,
-    });
+    await this.compile(true).next();
   }
+
+  private async *compile(watch: boolean) {
+    const targetDescription = parseTargetString(this.buildTarget, this.graph);
+    const executorContext = createExecutorContext(this.graph, targetDescription);
+    for await (const output of await runExecutor(targetDescription, { watch }, executorContext)) {
+      yield output;
+    }
+  }
+}
+
+function getAbsoluteOutputPath(graph: ProjectGraph, buildTarget: string) {
+  const targetDescription = parseTargetString(buildTarget, graph);
+  const outputPath = getTargetOption<string>(graph, targetDescription, 'outputPath');
+
+  return join(workspaceRoot, outputPath);
+}
+
+function getTargetOption<T>(graph: ProjectGraph, targetDescriptor: Target, option: string): T {
+  const { project, target, configuration } = targetDescriptor;
+  const targetConfiguration = graph.nodes[project].data.targets[target];
+  const defaultConfiguration = targetConfiguration.defaultConfiguration;
+  const normalizedConfiguration = configuration ?? defaultConfiguration;
+
+  return (
+    targetConfiguration.options[option] ??
+    targetConfiguration.configurations[normalizedConfiguration]?.[option]
+  );
+}
+
+/**
+ * Based on:
+ * @link https://github.com/nrwl/nx/blob/2e6592c533580aba7396fc910f92a7dbbf16163f/packages/cypress/src/utils/ct-helpers.ts#L106-L128
+ */
+function createExecutorContext(graph: ProjectGraph, targetDescriptor: Target): ExecutorContext {
+  const targets = graph.nodes[targetDescriptor.project]?.data.targets;
+  const projectsConfigurations = readProjectsConfigurationFromProjectGraph(graph);
+  // https://github.com/nrwl/nx/blob/717a8dd027991308e41cdf9ef3f370a352e65756/packages/nx/src/generators/utils/nx-json.ts#L15-L31
+  const nxJsonConfiguration = readNxJson();
+  return {
+    cwd: workspaceRoot,
+    projectGraph: graph,
+    target: targets[targetDescriptor.target],
+    targetName: targetDescriptor.target,
+    configurationName: targetDescriptor.configuration,
+    root: workspaceRoot,
+    isVerbose: false,
+    projectName: targetDescriptor.project,
+    projectsConfigurations,
+    nxJsonConfiguration,
+  };
 }
