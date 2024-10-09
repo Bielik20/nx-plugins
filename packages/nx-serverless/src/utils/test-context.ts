7e0e472755b30b0079e81f5bc6afe8ab2cec17e7
Filip Miklas <Filip.Miklas@wizards.com>
2024-10-09 11:21:09 +0200

    fix: adjust usage of executor context

--- packages/nx-serverless/src/utils/test-context.ts
+++ packages/nx-serverless/src/utils/test-context.ts
@@ -1,9 +1,11 @@
 import { ExecutorContext } from '@nx/devkit';
 
 export const testContext: ExecutorContext = {
+  projectGraph: {} as any, // TODO fill with data
+  nxJsonConfiguration: {} as any, // TODO fill with data
   root: '/base/ns3/tmp/nx-e2e/proj',
   target: { executor: '@ns3/nx-serverless:build' },
-  workspace: {
+  projectsConfigurations: {
     version: 2,
     projects: {
       serverlessMock: {
